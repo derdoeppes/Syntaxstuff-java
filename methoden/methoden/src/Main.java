654374e62706c9dd0171b1fa1c9506678c28a55f
derdoeppes <153041226+derdoeppes@users.noreply.github.com>
2024-03-28 01:30:13 +0100

    Update Main.java

--- methoden/methoden/src/Main.java
+++ methoden/methoden/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     System.out.println("Vor dem Methodenaufruf");
     DoSomething();                                          //wenn ich das Programm starte wird von oben nach unten alles ausgeführt
-    System.out.println("Nach dem Methodenaufruf");           // println ist auch eine methode die automatisch vordefiniert ist. wir müssen aber Parameter zuweisen
+    System.out.println("Nach dem Methodenaufruf");          // println ist auch eine methode die automatisch vordefiniert ist. wir müssen aber Parameter zuweisen
     System.out.println("Zweite Methode:");
     DoSomething2(10, 20);
 
@@ -18,11 +18,11 @@ public class Main {
         System.out.println(result);
     }
 
-    // hier noch mal die gleiche Methode aber wir weisen in der main Methode schon zwei Werte zu
 
 
 
-    public static void DoSomething2(int number1, int number2 ) {        
+
+    public static void DoSomething2(int number1, int number2 ) {        // hier noch mal die gleiche Methode aber wir weisen in der main Methode schon zwei Werte zu
         int a = number1;
         int b = number2;
         int result2 =  a + b;
